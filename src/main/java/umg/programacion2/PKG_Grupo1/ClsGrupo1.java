b3708c81ea9d48452e16b804801116ad0823235b
Marvin Cámbara <marvin75205@gmail.com>
2024-07-20 10:59:33 -0600

    En proceso de mejoras

--- src/main/java/umg/programacion2/PKG_Grupo1/ClsGrupo1.java
+++ src/main/java/umg/programacion2/PKG_Grupo1/ClsGrupo1.java
@@ -1,9 +1,18 @@
 package umg.programacion2.PKG_Grupo1;
 
 public class ClsGrupo1 {
+    public float pi = 3.1416f;
+
     public float radio (float radio)
     {
-        float pi = 3.1416f;
         return pi*(radio*radio);
+    }
+    public float cirunferencia(float radio)
+    {
+        return 2*pi*radio;
+    }
+    public float volumencubo(float lados)
+    {
+        return lados*lados*lados;
     }
 }
--- src/main/java/umg/programacion2/PKG_Grupo2/ClsGrupo2.java
+++ src/main/java/umg/programacion2/PKG_Grupo2/ClsGrupo2.java
@@ -4,4 +4,11 @@ public class ClsGrupo2 {
     public float triarea(float base, float altura) {
         return (base*altura)/2;
     }
+    public float piravol(float base, float altura)
+    {
+        return (base*altura)/3;
+    }
+    public float repiarea(float base, float altura) {
+        return (base*altura)/2;
+    }
 }
